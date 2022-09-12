7b990983137695f3d1b3929d88a78e3b5cefcc18
Sponkinho <swan.duval95@gmail.com>
2022-09-12 11:59:08 +0200

    create entities

---
+++ src/Entity/Chambre.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ChambreRepository;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+
+#[ORM\Entity(repositoryClass: ChambreRepository::class)]
+class Chambre
+{
+    use TimestampableEntity;
+    use SoftDeleteableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $descriptionCourte = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $descriptionLongue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $prixJournalier = null;
+
+    #[ORM\OneToMany(mappedBy: 'chambre', targetEntity: Commande::class)]
+    private Collection $commandes;
+
+    public function __construct()
+    {
+        $this->commandes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescriptionCourte(): ?string
+    {
+        return $this->descriptionCourte;
+    }
+
+    public function setDescriptionCourte(string $descriptionCourte): self
+    {
+        $this->descriptionCourte = $descriptionCourte;
+
+        return $this;
+    }
+
+    public function getDescriptionLongue(): ?string
+    {
+        return $this->descriptionLongue;
+    }
+
+    public function setDescriptionLongue(string $descriptionLongue): self
+    {
+        $this->descriptionLongue = $descriptionLongue;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getPrixJournalier(): ?string
+    {
+        return $this->prixJournalier;
+    }
+
+    public function setPrixJournalier(string $prixJournalier): self
+    {
+        $this->prixJournalier = $prixJournalier;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commande>
+     */
+    public function getCommandes(): Collection
+    {
+        return $this->commandes;
+    }
+
+    public function addCommande(Commande $commande): self
+    {
+        if (!$this->commandes->contains($commande)) {
+            $this->commandes->add($commande);
+            $commande->setChambre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommande(Commande $commande): self
+    {
+        if ($this->commandes->removeElement($commande)) {
+            // set the owning side to null (unless already changed)
+            if ($commande->getChambre() === $this) {
+                $commande->setChambre(null);
+            }
+        }
+
+        return $this;
+    }
+}
