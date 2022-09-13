aaaf15c9e0457a8f5f3032198ffc561c161b5a5f
hakim-boop <hh.infographie@gmail.com>
2022-09-13 18:36:30 +0200

    create-backoffice_chambre

--- src/Controller/ChambreController.php
+++ src/Controller/ChambreController.php
@@ -2,16 +2,91 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Chambre;
+use App\Form\ChambreFormType;
+use App\Repository\ChambreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 class ChambreController extends AbstractController
 {
-    #[Route('/voir-chambre', name: 'default_chambre', methods: ['GET'])]
-    public function showChambreById(): Response
-    {
+     #[Route('/voir-chambre', name: 'show_chambre', methods: ['GET'])]   
+     public function showChambre( ChambreRepository $repository, EntityManagerInterface $entityManager): Response
+        {
+        $chambre = $entityManager->getRepository(Chambre::class)->findBy(['deletedAt' => null]);
 
-        return $this->render('room/show_room.html.twig', []);
-    }
+        return $this->render('chambre/show_chambre.html.twig', [
+            'chambre' => $chambre,
+
+        ]);
+
+        } // end if $chambre
+        
+        #[Route('/ajouter-une-chambre', name: 'create_chambre', methods: ['GET', 'POST'])]
+        public function createChambre(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+        {
+            $chambre = new Chambre();
+    
+            $form = $this->createForm(ChambreFormType::class, $chambre)
+                ->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+    
+                $chambre->setCreatedAt(new DateTime);
+                $chambre->setUpdatedAt(new DateTime);
+    
+
+                $photo = $form->get('photo')->getData();
+    
+                if ($photo) {
+                  
+                    $this->handleFile($chambre, $photo, $slugger);
+                } // end if $photo
+    
+                $entityManager->persist($chambre);
+                $entityManager->flush();
+    
+                $this->addFlash('success', 'La chambre ajoutée avec succès !');
+                return $this->redirectToRoute('create_chambre');
+            } // end if $form
+    
+            return $this->render('chambre/create_chambre.html.twig', [
+                'form' => $form->createView()
+            ]);
+        } // end function create()
+
+
+////////////////////////////////////////////////////////  FONCTIONS CREES PAR NOUS MEME /////////////////////////////////////////////////////////////
+
+
+        private function handleFile(Chambre $chambre, UploadedFile $photo, SluggerInterface $slugger): void
+        {
+
+            $extension = '.' . $photo->guessExtension();
+    
+
+            $safeFilename = $slugger->slug($photo->getClientOriginalName());
+
+            $newFilename = $safeFilename . '_' . uniqid() . $extension;
+    
+  
+            try {
+
+                $photo->move($this->getParameter('uploads_dir'), $newFilename);
+
+                $chambre->setPhoto($newFilename);
+            } catch (FileException $exception) {
+                $this->addFlash('warning', 'La photo de la chambre ne s\'est pas importée avec succès. Veuillez réessayer.');
+                //            return $this->redirectToRoute('create_chambre');
+            }
+        } // end class handleFile()
+    
 }
---
+++ src/Form/ChambreFormType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chambre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ChambreFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('descriptionCourte', TextType::class, [
+                'label' => 'Sous-titre'
+            ])
+            ->add('descriptionLongue',  TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo',
+                'data_class' => null,
+                'constraints' => [
+                    new Image([
+                        'mimeTypes' => ['image/jpeg', 'image/png'],
+                        'mimeTypesMessage' => 'Les formats autorisés sont : .jpg, .png',
+                        'maxSize' => '3M',
+                        'maxSizeMessage' => "Le poids maximal du fichier est : {{ limit }} {{ suffix }} => {{ name }}: {{ size }} {{ suffix }}"
+                    ]),
+                ],
+                'help' => 'Fichiers autorisés: .jpg, .png',
+            ])
+            ->add('prixJournalier', TextType::class, [
+                'label' => 'Prix unitaire',
+            ])
+            ->add('submit', SubmitType::class, [
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto btn btn-primary col-3'
+                ],
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Chambre::class,
+        ]);
+    }
+}
