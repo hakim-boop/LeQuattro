c3602bb48c85de00e3754d5634d319815704816d
ChrisKhaled <98969088+ChrisKhaled@users.noreply.github.com>
2022-09-14 14:19:48 +0200

    show slider

--- src/Controller/SliderController.php
+++ src/Controller/SliderController.php
@@ -25,45 +25,45 @@ class SliderController extends AbstractController
         return $this->render('admin/show_slider.html.twig', [
             'slider' => $slider
         ]);
-        dd();
+
     } // end function show()
 
 
 
-    // #[Route('/ajouter-une-photo', name: 'create_slider', methods: ["GET", 'POST'])]
-    // public function createPhoto(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
-    // {
-    //     $slider = new Slider();
+    #[Route('/ajouter-une-photo', name: 'create_slider', methods: ["GET", 'POST'])]
+    public function createPhoto(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        $slider = new Slider();
 
-    //     $form = $this->createForm(SliderFormType::class, $slider)
-    //         ->handleRequest($request);
+        $form = $this->createForm(SliderFormType::class, $slider)
+            ->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
+        // if ($form->isSubmitted() && $form->isValid()) {
 
-    //         $slider->setCreatedAt(new DateTime);
-    //         $slider->setUpdatedAt(new DateTime);
+        //     $slider->setCreatedAt(new DateTime);
+        //     $slider->setUpdatedAt(new DateTime);
 
-    //         # On variabilise le fichier de la photo dans $photo.
-    //         # On obtient un objet de type UploadedFile()
-    //         /** @var UploadedFile $photo */
-    //         $photo = $form->get('photo')->getData();
+        //     # On variabilise le fichier de la photo dans $photo.
+        //     # On obtient un objet de type UploadedFile()
+        //     /** @var UploadedFile $photo */
+        //     $photo = $form->get('photo')->getData();
 
-    //         if ($photo) {
+        //     if ($photo) {
 
-    //             $this->handleFile($slider, $photo, $slugger);
-    //         } // end if $photo
+        //         $this->handleFile($slider, $photo, $slugger);
+        //     } // end if $photo
 
-    //         $entityManager->persist($slider);
-    //         $entityManager->flush();
+        //     $entityManager->persist($slider);
+        //     $entityManager->flush();
 
-    //         $this->addFlash('success', 'Vous avez ajouté une photo dans le carrousel !');
-    //         return $this->redirectToRoute('show_slider');
-    //     } // end if $form
+        //     $this->addFlash('success', 'Vous avez ajouté une photo dans le carrousel !');
+        //     return $this->redirectToRoute('show_slider');
+        // } // end if $form
 
-    //     return $this->render('admin/create_slider.html.twig', [
-    //         'form' => $form->createView()
-    //     ]);
-    // }
+        return $this->render('admin/create_slider.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
     // // ************************************ PRIVATE FUNCTION *********************************************
 
@@ -93,4 +93,5 @@ class SliderController extends AbstractController
     //         // return $this->redirectToRoute('create_produit');
     //     }
     // }
-}
+
+} //? end CLass
