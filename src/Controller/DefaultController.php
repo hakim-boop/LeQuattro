df91ad6b042308d761046e589d93065ee6ae94a7
ChrisKhaled <98969088+ChrisKhaled@users.noreply.github.com>
2022-09-16 09:55:38 +0200

    affichage carrousel

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Slider;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_home', methods:['GET'])]
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/home.html.twig');
+        $sliders = $entityManager->getRepository(Slider::class)->findBy(['deletedAt' => null]);
+
+        return $this->render('default/home.html.twig', [
+            'sliders' =>$sliders
+        ]);
             
     }
-
     
-}
+} //? end class
